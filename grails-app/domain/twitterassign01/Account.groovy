b9440bd3acced5f21906c07a3e4a2b5a161568b5
cwwaism <cwwacreations@gmail.com>
2016-02-07 15:14:56 -0600

    TweetMsg added Account property

--- grails-app/domain/twitterassign01/Account.groovy
+++ grails-app/domain/twitterassign01/Account.groovy
@@ -2,12 +2,19 @@ package twitterassign01
 
 class Account {
     String userHandle       // required field
-    String firstName        // required field
-    String lastName         // required field
+    String fullName        // required field
     String emailAddr        // required field
-    String phyAddr
+    String phoneNumber
     Date dateOfBirth
+    String accPassword      // required field
+
 
     static constraints = {
+        userHandle nullable:false, size: 0..30, unique: true
+        fullName nullable:false, blank: false
+        emailAddr nullable: false, blank: false, unique: true
+        phoneNumber nullable:true
+        dateOfBirth nullable:true
+        accPassword nullable: false, blank: false , size: 8..16
     }
 }
--- src/test/groovy/twitterassign01/AccountSpec.groovy
+++ src/test/groovy/twitterassign01/AccountSpec.groovy
@@ -1,22 +1,48 @@
 package twitterassign01
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Account)
+@Mock([Account])
+@Unroll
 class AccountSpec extends Specification {
 
     def setup() {
+
+
     }
+
+    def "Test required fields to ensure that constraints are enforced"() {
+        // def obj= new Account(userHandle:"",fullName:"",emailAddr:"",accPassword:"").save()
+        when:
+
+        def obj = new Account("field": val)
+
+        then:
+
+        validateConstraints(obj, field, error)
+
+        where:
+        error            | field         | val
+        'nulluserHandle' | 'userHandle'  | ''
+        'nulluserHandle' | 'fullName'    | ''
+        'nulluserHandle' | 'emailAddr'   | ''
+        'nulluserHandle' | 'accPassword' | ''
+
+    }
+
 
     def cleanup() {
     }
 
     void "test something"() {
-        expect:"fix me"
-            true == false
+        expect: "fix me"
+        true == false
     }
 }
